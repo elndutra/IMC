4140e9c76bc7bff7788e600a0deb149af4a38134
elndutra <elielton.dutra.morais@gmail.com>
2022-02-13 09:37:04 -0300

    Publicacao final do APP

--- IMC primeira Aula/ViewController.swift
+++ IMC primeira Aula/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var imResult: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    
+    
+    var imc: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calculate(_ sender: Any) {
+        
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!){
+            
+            imc = weight / (height*height)
+            showResults()
+        }
+    }
+    
+    func showResults(){
+        var result: String = " "
+        var image: String = " "
+    
+        switch imc{
+                case 0..<16:
+                    result = "Magreza"
+                    image = "abaixo"
+                case 16..<18.5:
+                    result = "Abaixo do peso"
+                    image = "abaixo"
+                case 18.5..<25:
+                    result = "Peso ideal"
+                    image = "ideal"
+                case 25..<30:
+                    result = "Sobre peso"
+                    image = "sobre"
+                default:
+                    result = "Obesidade"
+                    image = "obesidade"
+        }
+        
+        lbResult.text = result
+        imResult.image = UIImage(named: image)
+        viResult.isHidden = false
+    }
 }
-
